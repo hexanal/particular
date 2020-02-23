3b750532cc7afccb8cd5dd837d6eb6e31546cfd0
hexanal <frederic.mercy@gmail.com>
2020-02-23 09:37:52 -0500

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, and what's up?</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
